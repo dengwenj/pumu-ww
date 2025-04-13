bd48e07d89db65b0505e02a3bf513c01bb539a1b
朴睦 <927561237@qq.com>
2025-04-13 21:54:39 +0800

    基础配置

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+// 写的代码是否符合规范
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "@vue/typescript/recommended"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
---
+++ .prettierrc.js
@@ -0,0 +1,5 @@
+// 做代码格式化的
+module.exports = {
+  singleQuote: true, // 使用单引号
+  semi: false, // 使用分号
+}
